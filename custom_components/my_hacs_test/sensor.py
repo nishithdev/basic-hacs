d3e05cee1178e46c4073cd96cb771ee593952f4f
nsingdev <115286450+nishithdev@users.noreply.github.com>
2025-05-20 00:20:42 -0500

    Update sensor.py

--- custom_components/my_hacs_test/sensor.py
+++ custom_components/my_hacs_test/sensor.py
@@ -59,7 +59,6 @@ class CoServAccessTokenSensor(SensorEntity):
                         "status": status,
                         "status_code": response.status_code
                     }
-                    self.async_write_ha_state()  
                 else:
                     self._attr_native_value = "Login failed bad"
                     token = json_data.get("authorizationToken") # checking why token is not working in above step
